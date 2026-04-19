85bc4ba5bca3cb43177e6c63f27787badc36013b
Shaj2311 <haideralijafri2007@gmail.com>
2026-04-19 12:12:16 +0500

    Connect/disconnect from DB at startup/shutdown

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+import asyncpg
+from dotenv import load_dotenv
+import os
 from backend.routes import auth, users, rides, drivers, history, comms
 from backend.routes.staff import admin, superAdmin, support, staff
 
-app = FastAPI()
+# Load DB environment variables
+load_dotenv("db/.env")
 
+# Initialize API
+app = FastAPI()
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(rides.router)
@@ -14,6 +20,26 @@ app.include_router(admin.router)
 app.include_router(support.router)
 app.include_router(superAdmin.router)
 app.include_router(staff.router)
+
+
+# Connect to DB at startup
+@app.on_event("startup")
+async def startup():
+    print("Connecting!")
+    app.state.db = await asyncpg.create_pool(
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            database=os.getenv("POSTGRES_DB"),
+            host="localhost"
+            )
+
+
+# Disconnect from DB at shutdown
+@app.on_event("shutdown")
+async def shutdown():
+    print("Disconecting!")
+    await app.state.db.close()
+
 
 @app.get("/")
 async def root():
