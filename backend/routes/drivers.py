716d0daa3d776ae9e971cd0c8906bbceb41f4686
Shaj2311 <haideralijafri2007@gmail.com>
2026-04-19 17:22:57 +0500

    Complete drivers.py API calls

--- backend/routes/drivers.py
+++ backend/routes/drivers.py
@@ -6,7 +6,6 @@ router = APIRouter(prefix="/drivers", tags=["Drivers"])
 @router.get("/me")
 async def getDriverProfile(sessionKey: str, id: int, db = Depends(get_db)):
     async with db.acquire() as conn:
-        # 1. Query only the 'public' schema (where your tables live)
         query = """
             select d.driver_id DriverId, u.name, d.cnic, d.phone_no
             from Driver d
@@ -20,32 +19,30 @@ async def getDriverProfile(sessionKey: str, id: int, db = Depends(get_db)):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @router.get("/incomingRequests")
-async def checkIncomingRequests(sessionKey: str):
-    return {"pendingRequests": [{"rideId": 100, "distance": "1km"}]}
+async def checkIncomingRequests(sessionKey: str, db = Depends(get_db)):
+    async with db.acquire() as conn:
+        query = """
+            select trip_id, passenger_id, driver_id, start_time, end_time, pickup_loc, dropoff_loc, estimated_dist, actual_dist
+            from Trip
+            where start_time is NULL
+        """
+        rideRequests = await conn.fetch(query)
+        if not rideRequests:
+            return {"Error": "No incoming requests"}
+        return rideRequests
 
 
 @router.get("/incomingRequests/{id}")
-async def getRideRequestDetails(sessionKey: str, id: int):
-    return {"rideId": id, "distance": "1km"}
+async def getRideRequestDetails(sessionKey: str, id: int, db = Depends(get_db)):
+    async with db.acquire() as conn:
+        query = """
+            select passenger_id, driver_id, start_time, end_time, pickup_loc, dropoff_loc, estimated_dist, actual_dist
+            from Trip
+            where trip_id = $1
+            and start_time is NULL
+        """
+        rideRequest = await conn.fetchrow(query, id)
+        if not rideRequest:
+            return {"Error": "Request does not exist"}
+        return rideRequest
