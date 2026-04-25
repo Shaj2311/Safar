4c3d45cf48a322d511673db329d9496c08060e10
Muhammad Luqman <luqmandps@gmail.com>
2026-04-25 14:55:37 +0500

    Captain Panel added

--- frontend/AdminPanel/src/App.jsx
+++ frontend/AdminPanel/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import PassengerManagement from './PassengerManagement';
+import CaptainManagement from './CaptainManagement';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/passengers" element={<PassengerManagement />} />
+        <Route path="/captains" element={<CaptainManagement />} />
       </Routes>
     </Router>
   );
